1cc656f60c94b814a7e531ecb51912bd82ac0854
Yurii Yasnitskyi <yura.yasnitskiy@gmail.com>
2023-02-27 19:13:36 +0200

    maybe first commt

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-print('bla bla')
-server_id = 'abc123'
-
-if server_id == 'qqq':
-    print('DONE')
-else:
-    print('ERROR')
-
-login = input('Enter login: ')
-password = input('Enter password: ')
-
-if login == 'abc' and password == '123':
-    print('SUCCESS')
-
-input()
-input()
-
-x = 3
-y = 5
-print(x)
-z = 6
+name = input('Enter name: ')
+print(name)
