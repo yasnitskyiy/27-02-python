f96b62fba1e58ae6e7b4d875bd8bef165c7bd507
Yurii Yasnitskyi <yura.yasnitskiy@gmail.com>
2023-02-27 19:01:46 +0200

    some work

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ password = input('Enter password: ')
 if login == 'abc' and password == '123':
     print('SUCCESS')
 
+print('some work in test')
+print('some work in test')
+print('some work in test')
+print('some work in test')
+
